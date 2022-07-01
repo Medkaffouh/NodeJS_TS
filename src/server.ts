4ab3521261129f72af9ed63d59fe3e7b06068201
Medkaffouh <kaffouhmohamed@gmail.com>
2022-07-01 18:33:47 +0100

    comment utilisée les middleware

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express, {Request, Response} from "express";
+import serveStatic from "serve-static";
 export default class Server{
     constructor(private port:number) {
     }
     public start():void{
         const app=express();
+        //pour utiliser un middleware comme ça "app.use(xxx)"
+        app.use(serveStatic("public")); //pour specifier le dossier des ressources statiques
         app.get("/",(req:Request,resp :Response)=>{
             resp.send("Test ....");
         });
